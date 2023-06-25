c4384f2b1a551a43f9471ac6f9b2fbec4c3f3491
anandafiqri <anandafiqri1403@gmail.com>
2023-06-26 05:39:31 +0700

    init home page

---
+++ my-extension/src/views/home.tsx
@@ -0,0 +1,10 @@
+import { useEffect, useState } from "react";
+
+export default function Home(): JSX.Element {
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+
+  },[])
+  return <></>;
+}
